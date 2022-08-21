22114599a91113a89dbed359b606c8f6c02502ae
Connor Streng <connorstreng@connors-mbp.home>
2022-08-21 11:18:48 +0200

    Exercises 1-2 and 1-3

---
+++ chapter-01/1-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // print a table of fahr converted to celsius from f = 0 to f = 300
+    int lower, upper, step; 
+    float fahrenheit, celsius;
+    lower = 0;
+    upper = 300;
+    step = 20;
+    fahrenheit = lower;
+
+    printf(" F      C  \n");
+    printf("-----------\n");
+
+    while (fahrenheit <= upper) {
+        celsius = (5.0 / 9.0) * (fahrenheit - 32.0);
+        printf("%4.0f %6.1f\n", fahrenheit, celsius);
+        fahrenheit += step;
+    }
+
+    return 0;
+}
